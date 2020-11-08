90e2e3f9c66213cc3bc2f663869b09cf4fad117e
Pala2812 <Patricklange2812@gmail.com>
2020-11-08 23:05:04 +0100

    initial commit

---
+++ apps/volleymotion/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'volleymotion-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'volleymotion';
+}
