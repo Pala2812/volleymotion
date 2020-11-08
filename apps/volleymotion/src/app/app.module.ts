6157e49e79dc51eed0d5af340afc5b6d595140f0
Pala2812 <Patricklange2812@gmail.com>
2020-11-08 23:11:17 +0100

    install initial packages

--- apps/volleymotion/src/app/app.module.ts
+++ apps/volleymotion/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
