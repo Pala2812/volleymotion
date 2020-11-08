98aa0b447c1a08d066107d4bed086e370f1bc8fa
Pala2812 <Patricklange2812@gmail.com>
2020-11-08 23:13:35 +0100

    added packages

--- apps/volleymotion/src/app/app.module.ts
+++ apps/volleymotion/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, EffectsModule.forRoot([])],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(
+      {},
+      {
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictActionSerializability: true,
+          strictActionTypeUniqueness: true,
+          strictActionWithinNgZone: true,
+          strictStateImmutability: true,
+          strictStateSerializability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
