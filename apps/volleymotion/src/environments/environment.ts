3da8b5b4b322cc5ad0bdfb11700abd537d99ef9a
Pala2812 <Patricklange2812@gmail.com>
2020-11-12 11:30:57 +0100

    added dev environment

--- apps/volleymotion/src/environments/environment.ts
+++ apps/volleymotion/src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAtblMopFY9iloe6O2F5l8EYiB-0jbtNhQ',
-    authDomain: 'volley-motion.firebaseapp.com',
-    databaseURL: 'https://volley-motion.firebaseio.com',
-    projectId: 'volley-motion',
-    storageBucket: 'volley-motion.appspot.com',
-    messagingSenderId: '813157032610',
-    appId: '1:813157032610:web:d766771b83f9201db92dfb',
-    measurementId: 'G-SV9NXVT0R3',
+    apiKey: 'AIzaSyBbpBcwEmduSpq1tlGvJjetnVQ8Dh5aV50',
+    authDomain: 'volley-motion-dev.firebaseapp.com',
+    databaseURL: 'https://volley-motion-dev.firebaseio.com',
+    projectId: 'volley-motion-dev',
+    storageBucket: 'volley-motion-dev.appspot.com',
+    messagingSenderId: '122384843843',
+    appId: '1:122384843843:web:c137eefc8cdee29c8900f6',
   },
 };
