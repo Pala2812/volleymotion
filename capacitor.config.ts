500be62a239d65e4d8f790f586b336ceb94c1a0d
Patrick Lange <patricklange2812@gmail.com>
2021-07-19 16:46:35 +0200

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.volleymotion',
+  appName: 'volleymotion',
+  webDir: 'dist/apps/volleymotion',
+  bundledWebRuntime: false,
+};
+
+export default config;
